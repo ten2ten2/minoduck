0173a0db23203059396b9bb278d2837b31266e46
Ten^2 <lightning007zz@gmail.com>
2026-09-11 15:12:03 +0800

    Redirect settings root to general

---
+++ apps/console/app/pages/w/[workspaceSlug]/settings/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+const route = useRoute()
+await navigateTo(`/w/${String(route.params.workspaceSlug)}/settings/general`, { replace: true })
+</script>
+<template><div /></template>
